569ff35ef5d517ce32a3bdf6208c17a3237f20f9
Daniel D’Aquino <daniel@daquino.me>
2025-06-24 20:50:54 -0700

    Temporarily disable static site generation
    Static site generation is causing several issues, disabled for now, and
    setup netlify redirects to always route to the SPA

--- docsite/src/main.rs
+++ docsite/src/main.rs
@@ -28,46 +28,12 @@ enum Route {
     #[end_layout]
     #[end_layout]
     #[layout(MainLayout)]
-    #[route("/404")]
     #[route("/:..segments")]
     Err404 { segments: Vec<String> },
 }
 
-// The server function at the endpoint "static_routes" will be called by the CLI to generate the list of static
-// routes. You must explicitly set the endpoint to `"static_routes"` in the server function attribute instead of
-// the default randomly generated endpoint.
-#[server(endpoint = "static_routes", output = server_fn::codec::Json)]
-async fn static_routes() -> Result<Vec<String>, ServerFnError> {
-    // The `Routable` trait has a `static_routes` method that returns all static routes in the enum
-    Ok(Route::static_routes()
-        .iter()
-        .map(ToString::to_string)
-        .collect())
-}
-
 fn main() {
-    dioxus::LaunchBuilder::new()
-        // Set the server config only if we are building the server target
-        .with_cfg(server_only! {
-            ServeConfig::builder()
-                // Enable incremental rendering
-                .incremental(
-                    IncrementalRendererConfig::new()
-                        // Store static files in the public directory where other static assets like wasm are stored
-                        .static_dir(
-                            std::env::current_exe()
-                                .unwrap()
-                                .parent()
-                                .unwrap()
-                                .join("public")
-                        )
-                        // Don't clear the public folder on every build. The public folder has other files including the wasm
-                        // binary and static assets required for the app to run
-                        .clear_cache(false)
-                )
-                .enable_out_of_order_streaming()
-        })
-        .launch(App);
+    dioxus::launch(App);
 }
 
 #[component]
